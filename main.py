9ed0d63f9ed3b5dafa0f481dbdba98af82b5904c
JohnA28 <John.Alban41@gmail.com>
2024-06-19 18:31:32 -0400

    added user input

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-def add (num1, num2):
-  return num1 + num2
+operation = input('''
+            Please type in the operation you would like to complete.
+            + for addition
+            - for subtraction
+            * for multiplication
+            / for division
+            ''')
 
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the first number: '))
 
-def subtract(num1, num2):
-  return num1 - num2
+if operation == '+':
+  def add (num1, num2):
+    return num1 + num2
 
+elif operation == '-':
+  def subtract(num1, num2):
+    return num1 - num2
 
-def multiply(num1, num2):
-  return num1 * num2
+elif operation == '*':
+  def multiply(num1, num2):
+    return num1 * num2
 
-
-def division(num1, num2):
-  return num1/num2
+elif operation == '/':
+  def division(num1, num2):
+    return num1/num2
+  
+else: 
+  print('You have not typed a valid operator, please try again')
